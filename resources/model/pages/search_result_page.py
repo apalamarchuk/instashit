f25ec901b23613ac0ae574c45edc0bb3c3dbece1
Arthur Palamarchuk <apalamarchuk@cogniance.com>
2018-07-26 12:41:49 +0300

    add longer sleep

--- resources/model/pages/search_result_page.py
+++ resources/model/pages/search_result_page.py
@@ -18,10 +18,11 @@ class SearchResultPage(BasePage):
         self.object_rep.likes_link.click()
         self.object_rep.first_user_li.click()
         for i in range(1, 50):
-            sleep(3)
+            sleep(1)
             self.driver.find_element_by_xpath(self.object_rep.follow_button_xpath + "[" + str(i) + "]").click()
             name = str(self.driver.find_element_by_xpath(self.object_rep.follow_button_xpath + "[" + str(
                 i) + "]" + self.object_rep.follow_user_xpath).get_attribute("href"))
             mysql_helper.add_to_following_list(name)
             if i % 5 == 0:
                 self.driver.switch_to.active_element.send_keys(Keys.PAGE_DOWN)
+                sleep(3)
