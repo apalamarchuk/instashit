affca2039bd3540172615831d43887ada3ea3315
Arthur Palamarchuk <apalamarchuk@cogniance.com>
2018-08-06 09:56:33 +0300

    unfollowshit

--- unfollowshit.py
+++ unfollowshit.py
@@ -50,6 +50,7 @@ if following_number > 200:
             print("Can't unfollow shit")
             print("Len is " + str(len(following_list)))
             driver.quit()
+            exit(1)
         following_list.remove(link)
 
     print("Done")
